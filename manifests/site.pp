9720bfd6b866ea1bf8ebd649845221193ad7a0c2
Scott Wales <scott.wales@unimelb.edu.au>
2013-09-20 17:55:48 +1000

    Add vcs programs

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,13 +4,17 @@ node default {
         default_mods => false,
     }
 
+    package {["git", "subversion"]:}
+    
     vcsrepo { "/var/git/test":
         ensure => bare,
         provider => git,
+        require => Package["git"],
     }
 
     vcsrepo { "/var/svn/test":
         ensure => present,
         provider => svn,
+        require => Package["subversion"],
     }
 }
