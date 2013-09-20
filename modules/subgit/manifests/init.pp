87db12962d0dc5e73b8a8cad6c2c1c46ecd6c5a4
Scott Wales <scott.wales@unimelb.edu.au>
2013-09-20 18:39:07 +1000

    Add file pre-reqs

--- modules/subgit/manifests/init.pp
+++ modules/subgit/manifests/init.pp
@@ -7,9 +7,17 @@ define subgit ($git_url = $title, $svn_url) {
         creates => "$subgit_path",
     }
 
+    file {"$subgit_path":
+        require => Exec["/usr/bin/wget http://subgit.com/download/subgit-2.0.0.zip -O - | unzip"],
+    }
+
     exec {"$subgit_path/subgit configure --svn-url $svn_url $git_url":
         creates => "$git_url/subgit/config",
         require => [File["$subgit_path"],Class["java"]],
+    }
+
+    file {"$git_url/subgit/config":
+        require => Exec["$subgit_path/subgit configure --svn-url $svn_url $git_url"],
     }
 
     exec {"$subgit_path/subgit install $git_url":
