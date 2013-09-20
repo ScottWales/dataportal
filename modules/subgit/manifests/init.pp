39f3751f27026c554f8b2aa223959868999e6e54
Scott Wales <scott.wales@unimelb.edu.au>
2013-09-20 18:21:09 +1000

    Typo

--- modules/subgit/manifests/init.pp
+++ modules/subgit/manifests/init.pp
@@ -4,11 +4,11 @@ define subgit ($git_url = $title, $svn_url) {
     
     exec {"subgit configure --svn-url $svn_url $git_url":
         creates => "$git_url/subgit/config",
-        requires => Package["subgit"],
+        require => Package["subgit"],
     }
 
     exec {"subgit install $git_url":
         creates => "$git_url/hooks/pre-receive",
-        requires => File["$git_url/subgit/config"],
+        require => File["$git_url/subgit/config"],
     }
 }
