f62c0cce2c19accc0e899d5d5c5d1b9776bb60ea
Scott Wales <scott.wales@unimelb.edu.au>
2013-09-20 17:53:36 +1000

    Add some repositories

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,14 @@ node default {
     class {'apache':
         default_mods => false,
     }
+
+    vcsrepo { "/var/git/test":
+        ensure => bare,
+        provider => git,
+    }
+
+    vcsrepo { "/var/svn/test":
+        ensure => present,
+        provider => svn,
+    }
 }
