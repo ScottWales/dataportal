414a3f1a023f1c09d38bcbc1730af9bcb92fb1d5
Scott Wales <scott.wales@unimelb.edu.au>
2013-09-22 21:01:39 +1000

    Add proxy

--- modules/tomcat/manifests/init.pp
+++ modules/tomcat/manifests/init.pp
@@ -1,8 +1,13 @@
-class tomcat {
+class tomcat ($vhost = "*:80") {
     package {'tomcat6':}
 
     service {'tomcat6':
         require => Package['tomcat6'],
         ensure => running,
     }        
+
+    # Get apache to forward connections to tomcat
+    apache::vhost {$vhost:
+        proxy_dest => 'http://localhost:8080/',
+    }
 }
