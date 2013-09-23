7004286b619d91c0b37e5a679834cc479ff1fb3a
Scott Wales <scott.wales@unimelb.edu.au>
2013-09-23 10:33:44 +1000

    Cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,36 +15,17 @@
 
 node default {
 
+    # Bare-bones apache install
     class {'apache':
         default_mods => false,
         default_vhost => false,
     }
 
+    # Tomcat will be the default apache vhost
     class {'tomcat':}
 
+    # Dependencies
     class {'java':}
+    package {'subversion':}
 
-    package {["unzip","man","vim"]:}
-
-    package {["git", "subversion"]:}
-    file {["/var/git","/var/svn"]:
-        ensure => directory,
-    }
-    
-    vcsrepo { "/var/git/test":
-        ensure => bare,
-        provider => git,
-        require => [Package["git"],File["/var/git"]],
-    }
-
-    vcsrepo { "/var/svn/test":
-        ensure => present,
-        provider => svn,
-        require => [Package["subversion"],File["/var/svn"]],
-    }
-
-    subgit { "/var/git/sub":
-        svn_url => "file:///var/svn/test",
-        require => Vcsrepo["/var/svn/test"],
-    }
 }
