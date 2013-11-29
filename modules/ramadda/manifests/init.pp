c5ec90ef3173038c7a3a917c5d3edc31982fb5bf
Scott Wales <scott.wales@unimelb.edu.au>
2013-11-29 16:17:13 +1100

    Manage home dir

--- modules/ramadda/manifests/init.pp
+++ modules/ramadda/manifests/init.pp
@@ -1,18 +1,20 @@
 # Copyright 2013 ARC CoE for Climate System Science
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class ramadda {
+# Install Ramadda
+
+class ramadda (home = '/var/ramadda') {
     include tomcat
 
     vcsrepo {'/tmp/ramadda':
@@ -33,15 +35,20 @@ class ramadda {
     # Install
     file {'/var/lib/tomcat6/webapps/repository.war':
         source  => '/tmp/ramadda/dist/repository.war',
-        require => Exec['ant'],
+        require => [Exec['ant'],File[$ramadda::home]],
         notify  => Service['tomcat6'],
     }
 
-    # Home directory
-    file {'/usr/share/tomcat6/.ramadda':
-        ensure  => directory,
-        owner   => 'tomcat',
-        group   => 'tomcat',
-        require => Class['tomcat']
+    file {$ramadda::home:
+      ensure => directory,
+      owner  => 'tomcat',
+    }
+
+    # Configuration
+    file {'/usr/share/tomcat6/conf/repository.properties':
+        ensure   => present,
+        contents => "ramadda_home=${ramadda::home}",
+        owner    => 'tomcat',
+        require  => Class['tomcat'],
     }
 }
