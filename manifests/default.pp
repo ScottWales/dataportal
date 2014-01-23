401197197d3a6f8e43c130a7b7e354b14104d483
Scott Wales <scott.wales@unimelb.edu.au>
2014-01-23 12:01:33 +1100

    Update to use Hiera to control components

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,11 +1,11 @@
 # Copyright 2013 ARC CoE for Climate System Science
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,69 +13,79 @@
 # limitations under the License.
 
 
+hiera_include('classes')
+
 node default {
-  include ssh
-  include security
-  include sudo
-  include ec2user
-  include epel
+  #  $heira_classes = hiera_array('classes')
+  #  class {$heira_classes:}
+  include nci
 
-  mailalias {'forward root mail':
-    name      => 'root',
-    recipient => 'scott.wales@unimelb.edu.au',
+  $yum_packages = hiera_array('packages',[])
+  if size($yum_packages) > 0 {
+    package {$yum_packages:}
   }
 
-  # Firewall defaults
-  Firewall {
-    before  => Class['security::firewall_post'],
-    require => Class['security::firewall_pre'],
-  }
-
-  class {'monitoring':
-    monitor_ip => '128.250.120.34'
-  }
-
-  # Bare-bones apache install
-  class {'apache':
-    default_vhost => false,
-    default_mods  => false,
-  }
-
-  # Tomcat will be the default apache vhost
-  class {'tomcat':}
-
-  # Ramadda will be at http://$fqdn/repository
-  class {'ramadda':
-    vhost => 'climate-cms.nci.org.au',
-  }
-  class {'ramadda::ldap':
-    url             => 'ldap://sfldap0.anu.edu.au:389',
-    user_directory  => 'uid=${id},ou=People,dc=apac,dc=edu,dc=au',
-    group_directory => 'ou=Group,dc=apac,dc=edu,dc=au',
-    group_attribute => 'memberUid',
-    admin_group     => 'fe2_2',
-  }
-
-  # Dependencies
-  package{'wget':}
-
-  # Database
-  class {'postgresql::server':
-    postgres_password => 'test',
+  $pip_packages = hiera_array('python-packages',[])
+  if size($pip_packages) > 0 {
+    python::package {$pip_packages:}
   }
 
   if $::openstack_meta_site == 'NCI' {
     # NFS mounts
-    file {['/g','/g/data1','/g/data1/ua8']:
-      ensure => directory,
-    }
-    package {'nfs-utils':}
-    mount {'/g/data1/ua8':
-      ensure  => mounted,
-      device  => 'nnfs3.nci.org.au:/mnt/gdata1/ua8',
-      fstype  => 'nfs',
-      options => 'ro,nolock',
-      require => [Package['nfs-utils'],File['/g/data1/ua8']],
+    $gdata_mounts = hiera_array('gdata-mounts',[])
+    if size($gdata_mounts) > 0 {
+      nci::gdata {$gdata_mounts:}
     }
   }
-}
+
+  #  include ssh
+  #  include security
+  #  include sudo
+  #  include ec2user
+  #  include epel
+  #
+  #  mailalias {'forward root mail':
+  #    name      => 'root',
+  #    recipient => 'scott.wales@unimelb.edu.au',
+  #  }
+  #
+  #  # Firewall defaults
+  #  Firewall {
+  #    before  => Class['security::firewall_post'],
+  #    require => Class['security::firewall_pre'],
+  #  }
+  #
+  #  class {'monitoring':
+  #    monitor_ip => '128.250.120.34'
+  #  }
+  #
+  #  # Bare-bones apache install
+  #  class {'apache':
+  #    default_vhost => false,
+  #    default_mods  => false,
+  #  }
+  #
+  #  # Tomcat will be the default apache vhost
+  #  class {'tomcat':}
+  #
+  #  # Ramadda will be at http://$fqdn/repository
+  #  class {'ramadda':
+  #    vhost => 'climate-cms.nci.org.au',
+  #  }
+  #  class {'ramadda::ldap':
+  #    url             => 'ldap://sfldap0.anu.edu.au:389',
+  #    user_directory  => 'uid=${id},ou=People,dc=apac,dc=edu,dc=au',
+  #    group_directory => 'ou=Group,dc=apac,dc=edu,dc=au',
+  #    group_attribute => 'memberUid',
+  #    admin_group     => 'fe2_2',
+  #  }
+  #
+  #  # Dependencies
+  #  package{'wget':}
+  #
+  #  # Database
+  #  class {'postgresql::server':
+  #    postgres_password => 'test',
+  #  }
+  #
+  }
---
+++ modules/nci/manifests/gdata.pp
@@ -0,0 +1,34 @@
+## \file    modules/nci/manifests/mount.pp
+#  \author  Scott Wales <scott.wales@unimelb.edu.au>
+#  \brief
+#
+#  Copyright 2014 Scott Wales
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Mount a /g/data1 project
+define nci::gdata {
+  $mountpoint = "/g/data1/${title}"
+
+  file {$mountpoint:
+    ensure => directory,
+  }
+
+  mount {$mountpoint:
+    ensure  => mounted,
+    device  => "nnfs3.nci.org.au:/mnt/gdata1/${title}",
+    fstype  => 'nfs',
+    options => 'ro,nolock',
+    require => [Package['nfs-utils'],File[$mountpoint]],
+  }
+}
